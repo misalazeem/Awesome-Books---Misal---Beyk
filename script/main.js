d03db52eb789e9abaaf5ebb43351b6ded21885b4
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-04-24 20:09:12 +0500

    fixing linter errors

--- script/main.js
+++ script/main.js
@@ -1,30 +1,30 @@
 class Books {
-    constructor(title = '', author = ''){
-        this.title = title;
-        this.author = author;
-    }
+  constructor(title = '', author = '') {
+    this.title = title;
+    this.author = author;
+  }
 }
 let bookdata = [];
 document.addEventListener('DOMContentLoaded', () => {
-    window.onload = () => {
-      if (localStorage) {
-        const localStorageItem = localStorage.getItem('bookdata');
-        bookdata = JSON.parse(localStorageItem);
+  window.onload = () => {
+    if (localStorage) {
+      const localStorageItem = localStorage.getItem('bookdata');
+      bookdata = JSON.parse(localStorageItem);
       if (bookdata == null) {
         bookdata = [];
       }
-      const removebooksection = document.getElementById('removebooks');
-      for(let i = 0; i < bookdata.length; i += 1){
-        let bookhtml = `<form id="removebook${i}"><p>"${bookdata[i].title}"</p><p>${bookdata[i].author}</p><button id="removebookbutton${i}" type="submit">remove</button><hr></form>`
-        removebooksection.innerHTML += bookhtml;
-      }
-      for (let j = 0 ; j < bookdata.length ; j += 1) {
-        const removebutton = `#removebookbutton${j}`;
-        document.querySelector(removebutton).addEventListener('click', () => {
-          let prefix = removebutton[17];
-          bookdata.splice(prefix,1);
-        });
-      }
+    }
+    const removebooksection = document.getElementById('removebooks');
+    for(let i = 0; i < bookdata.length; i += 1){
+      let bookhtml = `<form id="removebook${i}"><p>"${bookdata[i].title}"</p><p>${bookdata[i].author}</p><button id="removebookbutton${i}" type="submit">remove</button><hr></form>`
+      removebooksection.innerHTML += bookhtml;
+    }
+    for (let j = 0 ; j < bookdata.length ; j += 1) {
+      const removebutton = `#removebookbutton${j}`;
+      document.querySelector(removebutton).addEventListener('click', () => {
+        let prefix = removebutton[17];
+        bookdata.splice(prefix,1);
+      });
     }
   };
 
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.getElementById('titlename').value != '' &&  document.getElementById('authorname').value) {
       bookobject.title = document.getElementById('titlename').value;
       bookobject.author = document.getElementById('authorname').value;
+      bookdata.push(bookobject);
     }
-    bookdata.push(bookobject);
   });
 });
