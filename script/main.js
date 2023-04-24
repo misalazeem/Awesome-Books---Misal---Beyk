0270e6f732f094585e097ca29fcf9506e08a39bc
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-04-24 20:00:52 +0500

    adding remove book funcitonality in JS file

--- script/main.js
+++ script/main.js
@@ -10,29 +10,35 @@ document.addEventListener('DOMContentLoaded', () => {
       if (localStorage) {
         const localStorageItem = localStorage.getItem('bookdata');
         bookdata = JSON.parse(localStorageItem);
-        if (bookdata == null) {
-            bookdata = [];
-        }
-        const removebooksection = document.getElementById('removebooks');
-        for(let i = 0; i < bookdata.length; i += 1){
-          let bookhtml = `<form id="removebook${i}"><p>"${bookdata[i].title}"</p><p>${bookdata[i].author}</p><button id="removebookbutton${i}" type="submit">remove</button><hr></form>`
-          removebooksection.innerHTML += bookhtml;
-        }
-    
+      if (bookdata == null) {
+        bookdata = [];
       }
-    };
-    
-    window.addEventListener('beforeunload', () => {
-        JSON.stringify(bookdata);
-        localStorage.setItem('bookdata', JSON.stringify(bookdata));
-    });
+      const removebooksection = document.getElementById('removebooks');
+      for(let i = 0; i < bookdata.length; i += 1){
+        let bookhtml = `<form id="removebook${i}"><p>"${bookdata[i].title}"</p><p>${bookdata[i].author}</p><button id="removebookbutton${i}" type="submit">remove</button><hr></form>`
+        removebooksection.innerHTML += bookhtml;
+      }
+      for (let j = 0 ; j < bookdata.length ; j += 1) {
+        const removebutton = `#removebookbutton${j}`;
+        document.querySelector(removebutton).addEventListener('click', () => {
+          let prefix = removebutton[17];
+          bookdata.splice(prefix,1);
+        });
+      }
+    }
+  };
 
-    document.querySelector('#addbookbutton').addEventListener('click', () => {
-      let bookobject = new Books();
-      if (document.getElementById('titlename').value != '' &&  document.getElementById('authorname').value) {
-        bookobject.title = document.getElementById('titlename').value;
-        bookobject.author = document.getElementById('authorname').value;
-      }
-      bookdata.push(bookobject);
-    });
+  window.addEventListener('beforeunload', () => {
+    JSON.stringify(bookdata);
+    localStorage.setItem('bookdata', JSON.stringify(bookdata));
+  });
+
+  document.querySelector('#addbookbutton').addEventListener('click', () => {
+    let bookobject = new Books();
+    if (document.getElementById('titlename').value != '' &&  document.getElementById('authorname').value) {
+      bookobject.title = document.getElementById('titlename').value;
+      bookobject.author = document.getElementById('authorname').value;
+    }
+    bookdata.push(bookobject);
+  });
 });
